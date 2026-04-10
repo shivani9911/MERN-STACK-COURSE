cc6d74513f047b8ebab27a37edce974fdfc8212d
shivani9911 <Shivanikamble9911@gmail.com>
2026-04-10 23:36:06 +0530

    done with change

---
+++ 2 Month Feb/React/hoc-website/src/components/EmailRow.jsx
@@ -0,0 +1,42 @@
+function EmailRow() {
+
+    return (
+        <>
+           <div className='email-list'>
+    <div className='email-row d-flex align-items-center border-bottom unread'>
+        <Form.Check type='checkbox' className='email-checkbox p-2' />
+        <button variant='link' className='star-btn p-0 btn p-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
+            </svg>
+        </button>
+
+        <span className='sender p-2'>Google Security</span>
+        <span className='subject flex-grow-1 p-2'>
+            Security Alert: New sign in detected.{""}
+            <span className='snippet'>
+                We noticed a new login to your account  on Windows.
+            </span>
+        </span>
+        <span className='time p-2'>9:42 AM</span>
+
+    </div>
+
+</div> 
+        </>
+    )
+}
+
+export default EmailRow
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2 Month Feb/React/hoc-website/src/screens/AboutUsPage.jsx
@@ -0,0 +1,11 @@
+function AboutUsPage() {
+
+    return (
+        <>
+            <h1>AboutUsPage </h1>
+            <EmailRow/>
+        </>
+    )
+}
+
+export default AboutUsPage 
