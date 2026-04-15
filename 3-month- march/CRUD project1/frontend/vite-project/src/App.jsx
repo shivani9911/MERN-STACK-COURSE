5463e18b10b929fa2ac6848506d8230572ae0a83
shivani9911 <Shivanikamble9911@gmail.com>
2026-04-15 22:06:36 +0530

    done with changes

---
+++ 3-month- march/CRUD project1/frontend/vite-project/src/App.jsx
@@ -0,0 +1,86 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+import "./style.css";
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-danger text-center my-5'>CRUD - MERN Stack Project Start</h2>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <h3 className='border text-center'>Add Item</h3>
+
+            <Form>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formGridAddress2">
+                <Form.Label>Address 2</Form.Label>
+                <Form.Control placeholder="Apartment, studio, or floor" />
+              </Form.Group>
+
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>State</Form.Label>
+                  <Form.Select defaultValue="Choose...">
+                    <option>Choose...</option>
+                    <option>...</option>
+                  </Form.Select>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Label>Zip</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3" id="formGridCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
+          <div className='col-md-6'>
+            <h3 className='border text-center'>Item List</h3>
+          </div>
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
+
